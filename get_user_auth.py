2c394bc45a1fcd32a14c32d6085d0070888512f1
David Todd <c0de@c0defox.es>
2018-04-24 23:37:46 -0500

    update to use new authorize method

--- get_user_auth.py
+++ get_user_auth.py
@@ -10,13 +10,9 @@ else:
     print "Usage: %s username" % (sys.argv[0],)
     sys.exit()
 
-token = util.prompt_for_user_token(username, scope)
+sp = util.authorize_api(username, scope, 'server')
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
-else:
-    print "Can't get token for", username
+results = sp.current_user_saved_tracks()
+for item in results['items']:
+    track = item['track']
+    print track['name'] + ' - ' + track['artists'][0]['name']
